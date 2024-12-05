71d10f4d2b189486a257dee98c5d650972aa7512
Dreu <mills1234a@gmail.com>
2024-12-05 01:03:07 -0800

    Cleaning Up

--- src/main/java/com/dreu/traversableleaves/mixin/LeavesBlockMixin.java
+++ src/main/java/com/dreu/traversableleaves/mixin/LeavesBlockMixin.java
@@ -1,7 +1,6 @@
 package com.dreu.traversableleaves.mixin;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
@@ -10,7 +9,6 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
