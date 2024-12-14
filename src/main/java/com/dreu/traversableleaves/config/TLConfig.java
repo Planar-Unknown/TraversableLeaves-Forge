5582316fa4d78914f25e05c1f123c6cfa3be2855
Dreu <52327746+SilentStrange@users.noreply.github.com>
2024-12-13 21:08:23 -0800

    Add files via upload

---
+++ src/main/java/com/dreu/traversableleaves/config/TLConfig.java
@@ -0,0 +1,156 @@
+package com.dreu.traversableleaves.config;
+
+import com.electronwill.nightconfig.core.Config;
+import com.electronwill.nightconfig.toml.TomlParser;
+import net.minecraft.resources.ResourceLocation;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static com.dreu.traversableleaves.TraversableLeaves.LOGGER;
+import static com.dreu.traversableleaves.TraversableLeaves.MODID;
+
+@SuppressWarnings({"SameParameterValue", "unchecked"})
+public class TLConfig {
+    public static boolean configNeedsRepair = false;
+    static final String fileName = "config/" + MODID + "/general.toml";
+    static final String defaultConfig = """
+           #To reset this config to default, delete this file and rerun the game.
+           #Movement Speed penalty while traversing leaves, 0 = no penalty (Range : 0 - 100)
+           SpeedPenalty = 27 #Default: 27
+           
+           #Whether Armor value reduces movement penalty
+           ArmorBonus = true #Defualt: true
+           
+           #List of leaves (false = Blacklist)
+           LeavesWhitelist = true #Default: true
+           Traversable=[
+            "minecraft:jungle_leaves",
+            "minecraft:oak_leaves",
+            "minecraft:spruce_leaves",
+            "minecraft:dark_oak_leaves",
+            "minecraft:acacia_leaves",
+            "minecraft:birch_leaves",
+            "minecraft:azalea_leaves",
+            "minecraft:flowering_azalea_leaves",
+            "minecraft:mangrove_leaves",
+            "minecraft:cherry_leaves",
+            "minecraft:pale_oak_leaves"
+           ]
+           
+           #List of Entities that can/cannot traverse leaves (false = Blacklist)
+           EntityWhitelist = false #Default: false
+           Entities=[
+            "minecraft:sheep",
+            "minecraft:pig",
+            "minecraft:cow",
+            "minecraft:chicken",
+            "minecraft:donkey",
+            "minecraft:llama",
+            "minecraft:trader_llama",
+            "minecraft:mule",
+            "minecraft:frog",
+            "minecraft:goat",
+            "minecraft:mooshroom",
+            "minecraft:turtle"
+           ]
+           """;
+    private static final Config CONFIG = parseFileOrDefault();
+    private static final Config DEFAULT_CONFIG = new TomlParser().parse(defaultConfig);
+
+    public static final Set<ResourceLocation> LEAVES = new HashSet<>();
+    public static final Set<ResourceLocation> ENTITIES = new HashSet<>();
+    private static final int CACHED_SPEED_PENALTY = getOrDefault("SpeedPenalty", Integer.class);
+    public static final float MOVEMENT_PENALTY = CACHED_SPEED_PENALTY * 0.02f;
+    public static final float ARMOR_SCALE_FACTOR = (2 - MOVEMENT_PENALTY) * 0.05f;
+    public static final boolean ARMOR_HELPS = getOrDefault("ArmorBonus", Boolean.class);
+    public static final boolean IS_LEAVES_WHITELIST = getOrDefault("LeavesWhitelist", Boolean.class);
+    public static final boolean IS_ENTITIES_WHITELIST = getOrDefault("EntityWhitelist", Boolean.class);
+    static {
+        List<String> leafStrings = getOrDefault("Traversable", List.class);
+        leafStrings.forEach((leaf) -> LEAVES.add(ResourceLocation.parse(leaf)));
+
+        List<String> entityStrings = getOrDefault("Entities", List.class);
+        entityStrings.forEach((entity) -> ENTITIES.add(ResourceLocation.parse(entity)));
+    }
+
+    static <T> T getOrDefault(String key, Class<T> clazz) {
+        try {
+            if ((CONFIG.get(key) == null)) {
+                LOGGER.error("Key [{}] is missing from Config: [{}] | Marking config file for repair...", key, fileName);
+                configNeedsRepair = true;
+                return clazz.cast(DEFAULT_CONFIG.get(key));
+            }
+            return clazz.cast(CONFIG.get(key));
+        } catch (Exception e) {
+            LOGGER.error("Value: [{}] for [{}] is an invalid type in Config: {} | Expected: [{}] but got: [{}] | Marking config file for repair...", CONFIG.get(key), key, fileName, clazz.getTypeName(), CONFIG.get(key).getClass().getTypeName());
+            configNeedsRepair = true;
+            return clazz.cast(DEFAULT_CONFIG.get(key));
+        }
+    }
+
+    static Config parseFileOrDefault() {
+        try {
+            Files.createDirectories(Path.of("config/" + MODID));} catch (Exception ignored) {}
+        return new TomlParser().parse(Path.of(fileName).toAbsolutePath(),
+                ((path, configFormat) -> {
+                    FileWriter writer = new FileWriter(path.toFile().getAbsolutePath());
+                    writer.write(defaultConfig);
+                    writer.close();
+                    return true;}));
+    }
+
+    public static void repairConfig() {
+        LOGGER.info("An issue was found with config: {} | You can find a copy of faulty config at: {} | Repairing...", fileName, fileName.replace(".toml", "_faulty.toml"));
+        Path sourcePath = Paths.get(fileName);
+        Path destinationPath = Paths.get(fileName.replace(".toml", "_faulty.toml"));
+        try {
+            Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            LOGGER.warn("Exception during faulty config caching: {}", e.getMessage());
+        }
+        try (FileWriter writer = new FileWriter(new File(fileName).getAbsolutePath())) {
+            StringBuilder contents = new StringBuilder("#Movement Speed penalty while traversing leaves, 0 = no penalty (Range : 0 - 100)\n");
+            contents.append("SpeedPenalty = ")
+                    .append(CACHED_SPEED_PENALTY)
+                    .append(" #Default: 27\n")
+                    .append("\n")
+                    .append("#Whether Armor value reduces movement penalty\n")
+                    .append("ArmorBonus = ")
+                    .append(ARMOR_HELPS)
+                    .append(" #Defualt: true\n")
+                    .append("\n")
+                    .append("#List of leaves (false = Blacklist)\n")
+                    .append("LeavesWhitelist = ")
+                    .append(IS_LEAVES_WHITELIST)
+                    .append(" #Default: true\n")
+                    .append("Traversable=[\n");
+            for (ResourceLocation rL : LEAVES) {
+                contents.append("   \"").append(rL.toString()).append("\",\n");
+            }
+            contents.append("""
+                    ]
+                    
+                    #List of Entities that can/cannot traverse leaves (false = Blacklist)
+                    EntityWhitelist =\s""")
+                    .append(IS_ENTITIES_WHITELIST)
+                    .append(" #Default: false\n")
+                    .append("Entities=[\n");
+            for (ResourceLocation rL : ENTITIES) {
+                contents.append("   \"").append(rL.toString()).append("\",\n");
+            }
+            contents.append("]\n");
+            writer.write(contents.toString());
+        } catch (IOException e) {
+            LOGGER.warn("Exception during config repair: {}", e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/dreu/traversableleaves/mixin/LeavesBlockMixin.java
@@ -0,0 +1,86 @@
+package com.dreu.traversableleaves.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.BlockParticleOption;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.LeavesBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.EntityCollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import static com.dreu.traversableleaves.config.TLConfig.*;
+import static net.minecraft.world.level.block.LeavesBlock.*;
+
+@Mixin(LeavesBlock.class) @SuppressWarnings({"unused"})
+public abstract class LeavesBlockMixin extends Block {
+    public LeavesBlockMixin(Properties properties) {
+        super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(DISTANCE, 7).setValue(PERSISTENT, false).setValue(WATERLOGGED, false));
+    }
+
+    @ParametersAreNonnullByDefault
+    public @NotNull VoxelShape getCollisionShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext context) {
+        if (context instanceof EntityCollisionContext entityContext && entityContext.getEntity() != null) {
+            return this.isTraversable() && IS_ENTITIES_WHITELIST == ENTITIES.contains(ForgeRegistries.ENTITY_TYPES.getKey(entityContext.getEntity().getType())) && !(context.isAbove(Shapes.block(), blockPos, true) && !context.isDescending()) ? Shapes.empty() : Shapes.block();
+        }
+        return Shapes.block();
+    }
+    @ParametersAreNonnullByDefault
+    public void entityInside(BlockState blockState, Level level, BlockPos blockPos, Entity entity) {
+        if (!isTraversable()) return;
+        if (entity instanceof Player player){
+            if (!level.getBlockState(new BlockPos(player.blockPosition())).is(BlockTags.LEAVES)){
+                entity.setDeltaMovement(entity.getDeltaMovement().multiply((MOVEMENT_PENALTY + getArmorBonus(player)) * 0.5f, 1, (MOVEMENT_PENALTY + getArmorBonus(player)) * 0.5f));
+            } else {
+                player.makeStuckInBlock(blockState, new Vec3(MOVEMENT_PENALTY + getArmorBonus(player), 1.0, MOVEMENT_PENALTY + getArmorBonus(player)));
+            }
+            createAmbience(player, blockPos);
+        } else if (entity instanceof LivingEntity) {
+            createAmbience(entity, blockPos);
+            entity.makeStuckInBlock(blockState, new Vec3(MOVEMENT_PENALTY, 1.0, MOVEMENT_PENALTY));
+        }
+    }
+
+    private float getArmorBonus(Player player) {
+        return ARMOR_HELPS ? ARMOR_SCALE_FACTOR * Mth.clamp(player.getArmorValue(), 0, 20) : 0;
+    }
+
+    private void createAmbience(Entity entity, BlockPos blockPos){
+        if (!entity.position().equals(new Vec3(entity.xOld, entity.yOld, entity.zOld))) {
+            if (entity.level().getGameTime() % 15 == 1) {
+                entity.playSound(SoundEvents.GRASS_BREAK, 0.1f, 0.6f);
+            }
+            if (entity.level().getGameTime() % 4 == 1){
+                double d0 = (double) blockPos.getX() + entity.level().random.nextDouble();
+                double d1 = (double) blockPos.getY() + 1;
+                double d2 = (double) blockPos.getZ() + entity.level().random.nextDouble();
+                entity.level().addParticle(new BlockParticleOption(ParticleTypes.BLOCK, this.defaultBlockState()), d0, d1, d2, 0, 0, 0);
+            }
+        }
+    }
+
+    public boolean isLadder(BlockState state, LevelReader level, BlockPos pos, LivingEntity entity) {
+        return isTraversable() && entity instanceof Player player && !player.isCrouching();
+    }
+    public boolean isTraversable(){
+        return IS_LEAVES_WHITELIST == LEAVES.contains(ForgeRegistries.BLOCKS.getKey(this));
+    }
+}
