18a587c16ea37ec79e5f61da3c831877e7dc4dfd
Dreu <mills1234a@gmail.com>
2025-06-18 00:19:35 -0700

    Updated Config options

--- src/main/java/com/dreu/traversableleaves/config/TLConfig.java
+++ src/main/java/com/dreu/traversableleaves/config/TLConfig.java
@@ -27,30 +27,34 @@ import static com.dreu.traversableleaves.TraversableLeaves.MODID;
 
 @SuppressWarnings({"SameParameterValue", "unchecked", "DataFlowIssue"})
 public class TLConfig {
-  //Todo: properly rigor test this config
   public static boolean configNeedsRepair = false;
   static final String fileName = "config/" + MODID + "/general.toml";
   static final String DEFAULT_CONFIG_STRING = """
       # To reset this config to default, delete this file and rerun the game.
-      # Movement Speed penalty while traversing leaves, 0 = no penalty (Range : 0 - 100) | Default: 27
-      SpeedPenalty = 27
+      # Movement Speed penalty while traversing leaves, 0 = no penalty, 99 = 99% slower (Range: 0 - 99) | Default: 50
+      SpeedPenalty = 50
       
       # Whether Armor value reduces movement penalty | Default: true
-      ArmorBonus = true
+      ArmorHelps = true
+      
+      # How much armor is required to completely negate the SpeedPenalty (Range: 0 - 255) | Default: 20
+      ArmorCap = 20
+      
+      # Whether leaves behave like ladders | Default: true
+      CanClimb = true
       
       # List of traversable blocks. | Default: ["#minecraft:leaves"]
       Traversable=[
        "#minecraft:leaves"
       ]
       
-      # Whether the Entities list is a whitelist (true), or a blacklist (false) | Default: false
-      EntityWhitelist = false
+      # Whether Entities is a "WHITELIST" or a "BLACKLIST" | Default: "BLACKLIST"
+      Entities-Whitelist-Blacklist = "BLACKLIST"
       
       # List of Entities that can/cannot traverse blocks | Default: []
       Entities=[
       ]
       """;
-
   public static void repairConfig() {
     LOGGER.info("An issue was found with config: {} | You can find a copy of faulty config at: {} | Repairing...", fileName, fileName.replace(".toml", "_faulty.toml"));
     Path sourcePath = Paths.get(fileName);
@@ -62,27 +66,32 @@ public class TLConfig {
     }
     try (FileWriter writer = new FileWriter(new File(fileName).getAbsolutePath())) {
       StringBuilder contents = new StringBuilder()
-          .append("# To reset this config to default, delete this file and rerun the game.")
-          .append("# Movement Speed penalty while traversing leaves, 0 = no penalty (Range : 0 - 100) | Default: 27\n")
+          .append("# To reset this config to default, delete this file and rerun the game.\n")
+          .append("# Movement Speed penalty while traversing leaves, 0 = no penalty, 99 = 99% slower (Range : 0 - 99) | Default: 50\n")
           .append("SpeedPenalty = ")
           .append(CACHED_SPEED_PENALTY)
-          .append("\n")
+          .append("\n\n")
           .append("# Whether Armor value reduces movement penalty | Default: true\n")
-          .append("ArmorBonus = ")
-          .append(ARMOR_HELPS)
-          .append("\n")
+          .append("ArmorHelps = ")
+          .append(CACHED_ARMOR_HELPS)
+          .append("\n\n")
+          .append("# Whether leaves behave like ladders\n")
+          .append("CanClimb = ")
+          .append(CAN_CLIMB)
+          .append("\n\n")
           .append("# List of traversable blocks | Default: [\"#minecraft:leaves\"]\n")
-          .append("Traversable=[\n");
+          .append("Traversable = [\n");
       for (String key : BLOCKS_CACHE)
         contents.append("   \"").append(key).append("\",\n");
       contents.append("""
               ]
               
-              #List of Entities that can/cannot traverse blocks (false = Blacklist)
-              EntityWhitelist =\s""")
-          .append(IS_ENTITIES_WHITELIST)
-          .append(" #Default: false\n")
-          .append("Entities=[\n");
+              
+              # Whether Entities is a "WHITELIST" or a "BLACKLIST" | Default: "BLACKLIST"
+              Entities-Whitelist-Blacklist =\s""")
+          .append(IS_ENTITIES_WHITELIST ? "\"WHITELIST\"" : "\"BLACKLIST\"")
+          .append("\n\n#List of Entities that can/cannot traverse blocks | Default: []\n")
+          .append("Entities = [\n");
       for (String key : ENTITIES_CACHE)
         contents.append("   \"").append(key).append("\",\n");
       contents.append("]\n");
@@ -91,6 +100,7 @@ public class TLConfig {
       LOGGER.warn("Exception during config repair: {}", e.getMessage());
     }
   }
+
 
   private static final Config DEFAULT_CONFIG = new TomlParser().parse(DEFAULT_CONFIG_STRING);
   private static Config CONFIG;
@@ -103,13 +113,15 @@ public class TLConfig {
 
   public static boolean IS_ENTITIES_WHITELIST;
   private static int CACHED_SPEED_PENALTY;
-  public static float MOVEMENT_PENALTY;
+  public static float MOVEMENT_MULTIPLIER;
   public static float ARMOR_SCALE_FACTOR;
-  public static boolean ARMOR_HELPS;
+  public static boolean CACHED_ARMOR_HELPS;
+  public static boolean CAN_CLIMB;
 
   public static void parse() {
     CONFIG = parseConfigOrDefault();
     if (CONFIG.get("LeavesWhitelist") != null) {
+      LOGGER.info("Traversable Leaves config was outdated, your old config was stored away at: {{}} and a new template was created at: {{}}", fileName.replace(".toml", "_old.toml"), fileName);
       cacheAndOverwriteOldConfig();
       CONFIG = new TomlParser().parse(DEFAULT_CONFIG_STRING);
     }
@@ -129,11 +141,12 @@ public class TLConfig {
     BLOCKS_CACHE.clear();
     TL_BLOCKS.clear();
     TL_ENTITIES.clear();
-    CACHED_SPEED_PENALTY = getOrDefault("SpeedPenalty", Integer.class);
-    MOVEMENT_PENALTY = CACHED_SPEED_PENALTY * 0.02f;
-    ARMOR_SCALE_FACTOR = (2 - MOVEMENT_PENALTY) * 0.05f;
-    ARMOR_HELPS = getOrDefault("ArmorBonus", Boolean.class);
-    IS_ENTITIES_WHITELIST = getOrDefault("EntityWhitelist", Boolean.class);
+    CACHED_SPEED_PENALTY = getClampedSpeedPenalty();
+    MOVEMENT_MULTIPLIER = (100 - CACHED_SPEED_PENALTY) * 0.01f;
+    CACHED_ARMOR_HELPS = getOrDefault("ArmorHelps", Boolean.class);
+    ARMOR_SCALE_FACTOR = CACHED_ARMOR_HELPS ? (1 - MOVEMENT_MULTIPLIER) * (1.0f / getClampedArmorCap()) : 0;
+    IS_ENTITIES_WHITELIST = getWhitelistBlacklist();
+    CAN_CLIMB = getOrDefault("CanClimb", Boolean.class);
 
     Set<ResourceLocation> toRemove = new HashSet<>();
     ((List<String>) getOrDefault("Traversable", List.class)).forEach((configKey) -> {
@@ -186,6 +199,42 @@ public class TLConfig {
         }
     });
     TL_ENTITIES.removeAll(toRemove);
+
+    if (configNeedsRepair) repairConfig();
+  }
+
+  private static boolean getWhitelistBlacklist() {
+    String whitelistBlacklist = getOrDefault("Entities-Whitelist-Blacklist", String.class).toUpperCase();
+    if (whitelistBlacklist.equals("WHITELIST")) return true;
+    else if (whitelistBlacklist.equals("BLACKLIST")) return false;
+    else {
+      LOGGER.warn("Invalid value for Entities-Whitelist-Blacklist in Config: {} | Expected: \"WHITELIST\" or \"BLACKLIST\", but got: \"{}\" | Using default (\"BLACKLIST\")...", fileName, whitelistBlacklist);
+      return false;
+    }
+  }
+
+  private static int getClampedArmorCap() {
+    int armorCap = getOrDefault("ArmorCap", Integer.class);
+    if (armorCap < 0) {
+      LOGGER.warn("Invalid ArmorCap: '{}' in Config: {{}} | Must be greater than 0 | Setting to 0...", armorCap, fileName);
+      armorCap = 0;
+    } else if (armorCap > 255) {
+      LOGGER.warn("Invalid ArmorCap: '{}' in Config: {{}} | Must be less than 255 | Setting to 255...", armorCap, fileName);
+      armorCap = 255;
+    }
+    return armorCap;
+  }
+
+  private static int getClampedSpeedPenalty() {
+    int speedPenalty = getOrDefault("SpeedPenalty", Integer.class);
+    if (speedPenalty < 0) {
+      LOGGER.warn("Invalid SpeedPenalty: '{}' in Config: {{}} | Must be greater than 0 | Setting to 0...", speedPenalty, fileName);
+      speedPenalty = 0;
+    } else if (speedPenalty > 99) {
+      LOGGER.warn("Invalid SpeedPenalty: '{}' in Config: {{}} | Must be less than 99 | Setting to 99...", speedPenalty, fileName);
+      speedPenalty = 99;
+    }
+    return speedPenalty;
   }
 
   private static boolean isValidEntityTag(String tagId) {
@@ -264,12 +313,18 @@ public class TLConfig {
       Files.createDirectories(Path.of("config/" + MODID));
     } catch (Exception ignored) {
     }
-    return new TomlParser().parse(Path.of(fileName).toAbsolutePath(),
-        ((path, configFormat) -> {
-          FileWriter writer = new FileWriter(path.toFile().getAbsolutePath());
-          writer.write(DEFAULT_CONFIG_STRING);
-          writer.close();
-          return true;
-        }));
+    try {
+      return new TomlParser().parse(Path.of(fileName).toAbsolutePath(),
+          ((path, configFormat) -> {
+            FileWriter writer = new FileWriter(path.toFile().getAbsolutePath());
+            writer.write(DEFAULT_CONFIG_STRING);
+            writer.close();
+            return true;
+          }));
+    } catch (Exception e) {
+      LOGGER.error("Encountered exception during parsing of Config: {{}}, moving config to {{}} and rewriting to builtin default instead", fileName, fileName.replace(".toml", "_faulty.toml"));
+      configNeedsRepair = true;
+      return new TomlParser().parse(DEFAULT_CONFIG_STRING);
+    }
   }
 }
